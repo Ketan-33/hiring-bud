ae0733880e84021f1da840191336f7ff193ab471
Ketan Pardhi <ketanpardhi7@gmail.com>
2025-02-26 01:49:44 +0530

    add back buttons

--- src/app/(root)/candidate/page.tsx
+++ src/app/(root)/candidate/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import CandidateApplicationForm from "@/components/CandidateApplicationForm";
+import { useRouter } from "next/navigation";
+import { ArrowLeftIcon } from "lucide-react";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 min-h-screen py-2">
+      <ArrowLeftIcon onClick={() => router.back()} className="self-start mb-4 h-6 w-6 text-indigo-600 cursor-pointer" />
       <h1 className="text-4xl font-bold mb-6">Candidate Application Form</h1>
       <CandidateApplicationForm className="mt-6" />
     </div>
